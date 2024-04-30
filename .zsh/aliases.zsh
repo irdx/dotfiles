2edc635ee326ded37384446634714409213e1c71
Thiago Castro <thiago.castro@zenvia.com>
2024-04-29 22:51:49 -0300

    fixes

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,5 @@
 alias k="kubectl"
 alias h="helm"
 alias tf="terraform"
+alias kubectx='f() { kubectl config get-contexts -o name | fzf --height=30 | xargs kubectl config use-context ; } ; f'
+alias kubens='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
